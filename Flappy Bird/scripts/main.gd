a62f4e92892d06b14e88db65af038ca4beee1aeb
DareAnanas <nylo2005@gmail.com>
2024-06-16 10:13:33 +0300

    make pipes as area2d

--- Flappy Bird/scripts/main.gd
+++ Flappy Bird/scripts/main.gd
@@ -2,15 +2,24 @@ extends Node2D
 
 @onready var moving_group_1 = $MovingGroup1
 @onready var moving_group_2 = $MovingGroup2
+@onready var teleport = $teleport
+@onready var spawnpoint = $spawnpoint
+@onready var pipe_combination = $PipeCombination
+
 
 const SPEED = 75;
 const DIRECTION = -1;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	for child in moving_group_1.get_children():
-		child.position.x += SPEED * DIRECTION * delta
-
-	for child in moving_group_2.get_children():
-		child.position.x += SPEED * DIRECTION * delta
+	
+	if (moving_group_1.position.x <= teleport.position.x):
+		moving_group_1.position.x = moving_group_2.position.x + 512
+	if (moving_group_2.position.x <= teleport.position.x):
+		moving_group_2.position.x = moving_group_1.position.x + 512
+	
+	moving_group_1.position.x += SPEED * DIRECTION * delta
+	moving_group_2.position.x += SPEED * DIRECTION * delta
+	
+	pipe_combination.position.x += SPEED * DIRECTION * delta;
 
