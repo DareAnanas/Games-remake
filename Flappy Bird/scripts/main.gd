89b6e7811a7ba1408c5a9372581d56662b5c27c5
DareAnanas <nylo2005@gmail.com>
2024-06-18 10:23:02 +0300

    added pipe queue and успішно deleted pipe from a scene but not from a pipe queue

--- Flappy Bird/scripts/main.gd
+++ Flappy Bird/scripts/main.gd
@@ -9,15 +9,18 @@ var pipe_combination_scene = preload("res://scenes/pipe_combination.tscn")
 var pipe_scene = preload("res://scenes/pipe.tscn")
 var pipe_combination
 
+var pipe_queue = [];
+
 const SPEED = 75;
 const DIRECTION = -1;
 
 func _ready():
 	pipe_combination = pipe_combination_scene.instantiate();
-	pipe_combination.position.x = spawnpoint.position.x;
+	pipe_combination.position.x = spawnpoint.position.x - 64;
 	var pipe = pipe_scene.instantiate();
 	pipe_combination.add_child(pipe);
-	add_child(pipe_combination);
+	pipe_queue.append(pipe_combination);
+	add_child(pipe_queue.back());
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -30,7 +33,9 @@ func _process(delta):
 	moving_group_1.position.x += SPEED * DIRECTION * delta
 	moving_group_2.position.x += SPEED * DIRECTION * delta
 	
-	pipe_combination.position.x += SPEED * DIRECTION * delta;
+	#pipe_combination.position.x += SPEED * DIRECTION * delta;
 
 func _on_timer_timeout():
-	print("Idi")
+	#TODO треба зробити чергу
+	pipe_queue[0].queue_free()
+	#pass
