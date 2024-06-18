ee5c69a816ce31a8baa2e95fb399db58fb0bd25d
DareAnanas <nylo2005@gmail.com>
2024-06-18 09:19:29 +0300

    instantiate a pipe in ready function

--- Flappy Bird/scripts/main.gd
+++ Flappy Bird/scripts/main.gd
@@ -4,12 +4,21 @@ extends Node2D
 @onready var moving_group_2 = $MovingGroup2
 @onready var teleport = $teleport
 @onready var spawnpoint = $spawnpoint
-@onready var pipe_combination = $PipeCombination
 
+var pipe_combination_scene = preload("res://scenes/pipe_combination.tscn")
+var pipe_scene = preload("res://scenes/pipe.tscn")
+var pipe_combination
 
 const SPEED = 75;
 const DIRECTION = -1;
 
+func _ready():
+	pipe_combination = pipe_combination_scene.instantiate();
+	pipe_combination.position.x = spawnpoint.position.x;
+	var pipe = pipe_scene.instantiate();
+	pipe_combination.add_child(pipe);
+	add_child(pipe_combination);
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
