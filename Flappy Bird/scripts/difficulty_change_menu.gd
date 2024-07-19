bb04396e68216dff8a18d89073987e258784a02b
DareAnanas <nylo2005@gmail.com>
2024-07-19 14:51:53 +0300

    Made a flappy bird difficulty changer

---
+++ Flappy Bird/scripts/difficulty_change_menu.gd
@@ -0,0 +1,31 @@
+extends Control
+
+
+func goToMenu():
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+
+func _on_back_pressed():
+	goToMenu();
+
+func writeDifficulty(difficulty):
+	var config = ConfigFile.new();
+	
+	config.set_value("Game", "difficulty", difficulty);
+	
+	config.save("user://difficulty.cfg");
+
+func _on_easy_pressed():
+	writeDifficulty("easy");
+	goToMenu();
+
+func _on_normal_pressed():
+	writeDifficulty("normal");
+	goToMenu();
+
+func _on_hard_pressed():
+	writeDifficulty("hard");
+	goToMenu();
+
+func _on_insane_pressed():
+	writeDifficulty("insane");
+	goToMenu();
--- Flappy Bird/scripts/menu.gd
+++ Flappy Bird/scripts/menu.gd
@@ -1,10 +1,28 @@
 extends Control
 
+@onready var difficulty_label = $MarginContainer/VBoxContainer/DifficultyLabel
+
+func _ready():
+	var config = ConfigFile.new()
+	
+	var err = config.load("user://difficulty.cfg")
+	
+	if err != OK:
+		return
+		
+	var difficulty = config.get_value("Game", "difficulty");
+	
+	difficulty_label.text = "Difficulty: " + difficulty
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 	
 
+func _on_change_difficulty_pressed():
+	get_tree().change_scene_to_file("res://scenes/difficulty_change_menu.tscn")
+
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
