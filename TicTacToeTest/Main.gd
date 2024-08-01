dfab898d6369c7cb7e76e9345e0597392a2ee41d
DareAnanas <nylo2005@gmail.com>
2024-08-01 23:10:07 +0300

    made good vector loop

--- TicTacToeTest/Main.gd
+++ TicTacToeTest/Main.gd
@@ -2,13 +2,28 @@ extends Node2D
 
 const ORIGIN = Vector2(0, 0);
 
+
 func vectorLoop(point : Vector2, vector : Vector2):
 	var points = [];
-	for x in range(vector.x + 1):
-		for y in range(vector.y + 1):
-			var modifiedX = x + point.x;
-			var modifiedY = y + point.y;
-			points.append([modifiedX, modifiedY]);
+	
+	var loopXEnded = false;
+	var loopYEnded = false;
+	var x = ORIGIN.x;
+	var y = ORIGIN.y;
+	
+	while not loopXEnded or not loopYEnded:
+		var modifiedX = x + point.x;
+		var modifiedY = y + point.y;
+		points.append([modifiedX, modifiedY]);
+		if x < vector.x:
+			x += 1;
+		else:
+			loopXEnded = true;
+		if y < vector.y:
+			y += 1;
+		else:
+			loopYEnded = true;
+	
 	return points;
 
 # Called when the node enters the scene tree for the first time.
